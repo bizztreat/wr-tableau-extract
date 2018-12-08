9037da6dfbb72ed1ea33112783e3389100eaa1bd
Tomáš Votava <tomas.votava@bizztreat.com>
2018-12-08 23:32:58 +0100

    commit

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM quay.io/keboola/docker-custom-python:latest
 
 RUN python3 -m pip install --upgrade pip
 RUN python3 -m pip install tableauserverclient
+RUN sudo apt-get -y install python3-libxml2
 
 COPY . /code/
 WORKDIR /data/
--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import tableauserverclient as tsc
 import json
 import os
 import sys
+import time
+from xml.etree import ElementTree as ET
 
-if (not os.path.exists("/data/config.json")) and (not os.path.exists("/code/config.json")):
+conf_path = ("/data/config.json" if os.path.exists("/data/config.json") else ("/code/config.json" if os.path.exists("/code/config.json") else "config.json"))
+
+
+if (not os.path.exists(conf_path)):
 	print("Cannot run without configuration")
 	sys.exit(0)
 
-with open("/data/config.json" if os.path.exists("/data/config.json") else "/code/config.json", "r") as conf_file:
+with open(conf_path, "r") as conf_file:
 	conf = json.load(conf_file)["parameters"]
 
 
@@ -19,6 +24,29 @@ auth = tsc.TableauAuth(conf["user"], conf["#pass"])
 server = tsc.Server(conf["server"])
 
 with server.auth.sign_in(auth):
-    datasources, pitem = server.datasources.get()
-    print("\nThere are {} datasources on site: ".format(pitem.total_available))
-    print([datasource.name for datasource in datasources])
+	# use server API version
+	server.use_server_version()
+	
+	followed_tasks = {}
+	
+	# tasks
+	tasks, pitem = server.tasks.get()
+	for task in tasks:
+		if task.target.type!="datasource": continue
+		ds = server.datasources.get_by_id(task.target.id)
+		refresh_type = ("increment" if "increment" in task.task_type.lower() else "full")
+		if ds.name in conf["datasources"] and refresh_type==conf["type"]:
+			print("Starting {0} refresh of {1}".format(refresh_type,ds.name))
+			response = server.tasks.run(task)
+			root = ET.fromstring(response)
+			for chld in root:
+				print(chld.tag,chld.attrib)
+			followed_tasks[ds] = task
+			#sched = server.schedules.get()
+			#print(sched)
+	
+	while True:
+		time.sleep(5)
+		for ds in followed_tasks:
+			t = server.tasks.get_by_id(followed_tasks[ds].id)
+			print("{0} is currently {1}".format(ds,t.state))
