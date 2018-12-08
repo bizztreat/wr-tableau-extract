bbc9c92549a5e1aa6453ebd51acf75a6545bc56b
Tomáš Votava <tomas.votava@bizztreat.com>
2018-12-08 21:02:07 +0100

    initial commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import json
 import os
 import sys
 
-if (not os.path.exists("/data/config.json")) and (not os.path.exists("config.json")):
+if (not os.path.exists("/data/config.json")) and (not os.path.exists("/code/config.json")):
 	print("Cannot run without configuration")
 	sys.exit(0)
 
-with open("/data/config.json" if os.path.exists("/data/config.json") else "config.json", "r") as conf_file:
+with open("/data/config.json" if os.path.exists("/data/config.json") else "/code/config.json", "r") as conf_file:
 	conf = json.load(conf_file)
 
 print(conf)
