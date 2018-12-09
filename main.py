8a5bddc02899b151b1674902676c5825aed725dc
Tomáš Votava <tomas.votava@bizztreat.com>
2018-12-09 23:30:43 +0100

    First pre-release

--- main.py
+++ main.py
@@ -23,12 +23,25 @@ with open(conf_path, "r") as conf_file:
 auth = tsc.TableauAuth(conf["user"], conf["#pass"])
 server = tsc.Server(conf["server"])
 
+print("Connecting to server {0} using username \'{1}\'".format(conf["server"],conf["user"]))
+
 with server.auth.sign_in(auth):
+	print("Connection established.")
 	# use server API version
 	server.use_server_version()
 	
+	server_info = server.server_info.get()
+	print("Server REST API version: {0}".format(server_info.rest_api_version))
+	if (server_info.rest_api_version < '3.1'):
+		nopoll_mode = True
+		print("Running in no-poll mode, only servers using REST API version 3.1 and higher support that.")
+	else:
+		nopoll_mode = False
+		print("Running in poll mode.")
+		
 	followed_tasks = {}
 	
+	print("Getting tasks list.")
 	# tasks
 	tasks, pitem = server.tasks.get()
 	for task in tasks:
@@ -37,16 +50,28 @@ with server.auth.sign_in(auth):
 		refresh_type = ("increment" if "increment" in task.task_type.lower() else "full")
 		if ds.name in conf["datasources"] and refresh_type==conf["type"]:
 			print("Starting {0} refresh of {1}".format(refresh_type,ds.name))
-			response = server.tasks.run(task)
-			root = ET.fromstring(response)
-			for chld in root:
-				print(chld.tag,chld.attrib)
-			followed_tasks[ds] = task
-			#sched = server.schedules.get()
-			#print(sched)
-	
+			response = ""
+			#response = server.tasks.run(task)
+			if not nopoll_mode:
+				try: root = ET.fromstring(response)
+				except Exception as m:
+					print("Error parsing response the refresh task might not have been run, no way to know :/.\nSkipping poll-check for datasource {0}\nOriginal error: {1}".format(ds.name,str(m)))
+					continue
+				for chld in root:
+					try:
+						jid = chld.attrib["id"]
+						followed_tasks[ds.name] = jid
+					except Exception as e:
+						print("Job not found, skipping poll-check for datasource \'{0}\'.".format(ds.name))
+				#sched = server.schedules.get()
+				#print(sched)
+	if nopoll_mode or len(followed_tasks.keys()):
+		print("Nothing to poll or nowhere to poll from. Finished.")
+		sys.exit(0)
+	print("Continue in poll mode")
 	while True:
 		time.sleep(5)
 		for ds in followed_tasks:
-			t = server.tasks.get_by_id(followed_tasks[ds].id)
-			print("{0} is currently {1}".format(ds,t.state))
+			#j = server.jobs.get_by_id(followed_tasks[ds])
+			#print(dir(j))
+			print("I would normally poll job for datasource {0}, but this is only a debug.".format(ds))
